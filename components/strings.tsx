f24829e0212837118fa54e6fa161a87fd9b7dd13
Shie1 <sonkolyw@gmail.com>
2023-07-13 23:00:06 +0200

    Update

--- components/strings.tsx
+++ components/strings.tsx
@@ -1,6 +1,5 @@
 export function getVideoIDFromURL(url: string) {
     var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(shorts\/)|(embed\/)|(watch\?))\??v?=?([^#&?]*).*/;
     var match = url.match(regExp);
-    console.log(match)
     return (match && match[8].length == 11) ? match[8] : undefined;
 }
